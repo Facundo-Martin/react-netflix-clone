5bbc993b180feb5e8882f6dcd6bb513fa3e32d21
Facundo Martin <facundomartin98@gmail.com>
2023-01-04 12:13:29 -0800

    added firebase config nad cleaned up project

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCYolWgPTLsd5yMKlpuHQ8bKoYUTDdCUZ4",
+  authDomain: "react-netflix-clone-860e1.firebaseapp.com",
+  projectId: "react-netflix-clone-860e1",
+  storageBucket: "react-netflix-clone-860e1.appspot.com",
+  messagingSenderId: "196772853590",
+  appId: "1:196772853590:web:113b48077434bc457208f7",
+};
