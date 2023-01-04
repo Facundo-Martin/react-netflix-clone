d2b2f1320ee8579f6000510a9713db99f8170b05
Facundo Martin <facundomartin98@gmail.com>
2023-01-04 12:23:51 -0800

    added default imports for comopnents folder

--- src/containers/HomeScreen/index.jsx
+++ src/containers/HomeScreen/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { Banner, Navbar } from "../../components";
 
 const HomeScreen = () => {
   return (
     <div>
       {/* Navbar */}
-
+      <Navbar />
       {/* Banner */}
+      <Banner />
 
       {/* Row */}
     </div>
