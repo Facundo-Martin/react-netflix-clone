3f38f2c69660d5faeb404d91c5d8095051bfcc13
Facundo Martin <facundomartin98@gmail.com>
2023-01-05 10:16:54 -0800

    added Row layout

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Navbar } from "./HomeScreen/Navbar";
 export { default as Banner } from "./HomeScreen/Banner";
+export { default as Row } from "./HomeScreen/Row";
--- src/containers/HomeScreen/index.jsx
+++ src/containers/HomeScreen/index.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { Banner, Navbar } from "../../components";
+import { Banner, Navbar, Row } from "../../components";
+import requests from "../../lib/endpoints";
 
 const HomeScreen = () => {
   return (
     <div>
       <Navbar />
       <Banner />
-      {/* Row */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
+      <Row title="Action Movies" fetchUrl={requests.fetchTopRated} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 };
