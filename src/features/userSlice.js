cede44a499cacfa13aeee3252aaed47fcdcf22e9
Facundo Martin <facundomartin98@gmail.com>
2023-01-08 11:24:29 -0800

    created uRTK userSlice

---
+++ src/features/userSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { user: null };
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signIn(state, action) {
+      state.user = action.payload;
+    },
+    signOut(state) {
+      state.user = null;
+    },
+  },
+});
+
+export const { signIn, signOut } = userSlice.actions;
+export const selectUser = (state) => state.user.user;
+export default userSlice.reducer;
